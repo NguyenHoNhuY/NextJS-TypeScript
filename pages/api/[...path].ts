dd20a957762a9be0bc586a71131a06821e368867
NguyenHoNhuY <nguyenhonhuy1010@gmail.com>
2022-05-11 09:22:46 +0700

    redeploy proxy server 2

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -18,7 +18,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 	//* https://js-post-api.herokuapp.com/api/students
 
 	proxy.web(req, res, {
-		target: 'https://js-post-api.herokuapp.com',
+		target: process.env.API_URL,
 		changeOrigin: true,
 		selfHandleResponse: false,
 	});
